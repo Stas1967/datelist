cf3fc675a4553fd13db5e7908b9c840b613e3788
Tomasz <vizaint@hotmail.com>
2020-07-18 13:45:16 +0100

    datelist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+
+export interface Dni {
+  dzien: Date;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'datelist';
+  day: Date;
+  dateA: Date | null;
+  dateB: Date | null;
+  datelist: string[] = [];
+
+  constructor() {
+      this.day = new Date();
+      this.dateA = new Date();
+      this.dateB = new Date();
+      this.dateB.setDate(this.dateB.getDate() + 7);
+  }
+
+  addEvent1 = (event: MatDatepickerInputEvent<Date>) => {
+    this.datelist = [];
+    console.log('DateA', event.target.value);
+    const da = event.target.value
+    if (da != null) {
+      this.dateA = event.target.value;
+    }    
+  }
+
+  addEvent2 = (event: MatDatepickerInputEvent<Date>) => {
+    this.datelist = [];
+    console.log('DateB', event.target.value);
+    const db = event.target.value
+    if (db != null) {
+      this.dateB = event.target.value;
+    }
+  }
+    
+
+  getListOfDate = () => {
+    if (this.dateA && this.dateB) {
+      for (this.day = this.dateA; this.day <= this.dateB; this.day.setDate(this.day.getDate() + 1)) {
+        console.log(this.day);
+        this.datelist.push(this.day.toLocaleDateString('es'))
+      }
+    }
+    
+    
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatNativeDateModule, MatRippleModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -10,9 +21,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatNativeDateModule, MatRippleModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatExpansionModule,
+    MatInputModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
