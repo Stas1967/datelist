d188c65d8de9af2631025c2e76f4604a7bf8d77a
Tomasz <56225943+Stas1967@users.noreply.github.com>
2020-07-22 14:56:12 +0100

    Update navito.component.ts

--- src/app/navito/navito.component.ts
+++ src/app/navito/navito.component.ts
@@ -19,7 +19,7 @@ export class NavitoComponent implements OnInit {
     this.meta.addTags([
       { name: 'keywords', content: 'Another page meta' },
       { name: 'robots', content: 'index, follow' },
-      { name: 'author', content: 'Digamber Singh' },
+      { name: 'author', content: 'Diservitec' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
       { charset: 'UTF-8' }
